2db013cec4ced0d073e5cf564bc2abfdc09826f9
ghostnetrn <54438080+ghostnetrn@users.noreply.github.com>
2021-07-30 23:46:23 -0300

    Delete update.sh

--- update.sh
+++
@@ -1,7 +0,0 @@
-wget -N https://github.com/enarjord/passivbot/archive/refs/heads/master.zip
-unzip master.zip -o -x passivbot-master/api-keys.json  
-cp -r passivbot-master/* .
-git add .
-#git remote set-url origin git@github.com:ghostnetrn/botbinance.git
-git commit -m "update"
-git push -u origin main

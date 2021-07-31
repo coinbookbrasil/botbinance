b89b8f46e36d2c75989ee7012714a81bcfb5d7a9
Coinbook <coinbookbrasil@gmail.com>
2021-07-31 02:07:04 +0000

    update

--- update.sh
+++ update.sh
@@ -1,4 +1,6 @@
 wget -N https://github.com/enarjord/passivbot/archive/refs/heads/master.zip
 unzip master.zip -x passivbot-master/api-keys.json  
 cp -r passivbot-master/* .
-pip3 install -r requirements.txt
+git add .
+git commit -m "update"
+git push -u origin main

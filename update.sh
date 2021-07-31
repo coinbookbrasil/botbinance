f8b408100b51615847b8e3e5ef9b0ce24536b6a8
CoinBookBrasil <coinbookbrasil@gmail.com>
2021-07-31 17:36:23 +0000

    update

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 wget -N https://github.com/enarjord/passivbot/archive/refs/heads/master.zip
 #unzip master.zip -o -x passivbot-master/api-keys.json  
-unzip -o master.zip
+unzip -o master.zip -x passivbot-master/README.md
 cp -r passivbot-master/* .
 git add .
 #git remote set-url origin git@github.com:ghostnetrn/botbinance.git
